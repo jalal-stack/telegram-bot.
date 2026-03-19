61757e0138f482f85578e0e5a9af546cb842ea5e
jalal-stack <79417790+jalal-stack@users.noreply.github.com>
2026-03-20 02:17:53 +0500

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,3 @@ def webhook():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
-    return {"ok": True}
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
