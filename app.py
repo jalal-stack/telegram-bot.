118eb5c280627b3b33560f4cf842f925629cee98
jalal-stack <79417790+jalal-stack@users.noreply.github.com>
2026-03-20 02:12:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import requests
 import os
 
-app = Flask(__name__)  # 👈 ОБЯЗАТЕЛЬНО ВВЕРХУ
+app = Flask(__name__)
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
@@ -11,53 +11,56 @@ CHAT_ID = os.getenv("CHAT_ID")
 def home():
     return "WORKING"
 
+
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
+    print("🔥 WEBHOOK HIT")
+
     data = request.args
 
-parent = data.get('parent', '—')
-child = data.get('child', '—')
-age = data.get('age', '—')
-phone = data.get('phone', '—')
-address = data.get('address', '—')
-date = data.get('date', '—')
-complaint = data.get('complaint', '-') 
-stage = data.get('stage', '').lower()
+    parent = data.get('parent', '—')
+    child = data.get('child', '—')
+    age = data.get('age', '—')
+    phone = data.get('phone', '—')
+    address = data.get('address', '—')
+    date = data.get('date', '—')
+    complaint = data.get('complaint', '—')
+    stage = data.get('stage', '').lower()
 
-# 🔥 разные тексты
-if "жалоб" in stage:
-    text = f"""🚨 ЖАЛОБА!
+    # 🔥 разные тексты
+    if "жалоб" in stage:
+        text = f"""🚨 ЖАЛОБА!
 
-👩Ребёнок: {child}
+👶 Ребёнок: {child}
 👩 Родитель: {parent}
 📞 Телефон: {phone}
 
 📝 Жалоба:
 {complaint}
+"""
 
-elif "резерв" in stage:
-    text = f"""📌 РЕЗЕРВ:
+    elif "резерв" in stage:
+        text = f"""📌 РЕЗЕРВ:
 
-👩 ФИО ребенка: {child}
-2. ФИО родителя: {parent}
-3. Возраст ребенка: {age}
-4. Адрес желаемого садика: {address}
-5. Телефон номер родителя: {phone}
+👶 ФИО ребенка: {child}
+👩 ФИО родителя: {parent}
+🎂 Возраст ребенка: {age}
+📍 Адрес: {address}
+📞 Телефон: {phone}
+"""
 
+    else:
+        text = f"""👀 Заявка на Диагностику:
 
-
-else:
-    text = f"""👀 Заявка на Диагностику:
-
-1.  ФИО ребенка: {child}
+1. ФИО ребенка: {child}
 2. ФИО родителя: {parent}
 3. Возраст ребенка: {age}
 4. Адрес желаемого садика: {address}
 5. Телефон номер родителя: {phone}
 6. Дата и время пробного дня: {date}
+"""
 
-    print("🔥 WEBHOOK HIT:", data)  # лог
-
+    # 📲 отправка в Telegram
     requests.post(
         f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
         data={
@@ -66,6 +69,11 @@ else:
         }
     )
 
+    return {"ok": True}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
     return {"ok": True}
 
 if __name__ == "__main__":
