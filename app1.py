0c1540bfee2b087535291d18e848a6e69c7046f9
jalal-stack <79417790+jalal-stack@users.noreply.github.com>
2026-03-19 21:56:04 +0500

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,10 @@
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.args
+
+    parent = data.get('parent', '—')
+    child = data.get('child', '—')
+    age = data.get('age', '—')
+    phone = data.get('phone', '—')
+    address = data.get('address', '—')
+    date = data.get('date', '—')
